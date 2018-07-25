8e98e1de1fac667a3973b15066c6f74b0093390c
Aaron Mathews <aaron.mathews95@gmail.com>
2018-07-24 20:11:27 -0400

    Created user service and models

---
+++ src/app/models/exercise.ts
@@ -0,0 +1,11 @@
+export class Exercise {
+    exercise_id: number;
+    exercise_name: string;
+    type_id: number;
+    exercise_description: string;
+    exercise_sets: number;
+    exercise_reps: number;
+    exercise_duration: number;
+    user_id: number;
+    visibility: number;
+}
