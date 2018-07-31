57880f5ef487ff71acad3eba4cff5b7eb41f5b94
Aaron Mathews <aaron.mathews95@gmail.com>
2018-07-31 15:42:41 -0400

    Helping derek figure out his life

--- src/app/models/exercise.ts
+++ src/app/models/exercise.ts
@@ -6,20 +6,20 @@ export class Exercise {
     exercise_sets: number;
     exercise_reps: number;
     exercise_duration: number;
-    exercise_rest:number;
+    exercise_rest: number;
     user_id: number;
     visibility: number;
 
-    constructor (id: number, name: string, type: number, descr: string, sets: number,
-        reps: number, dura: number, user: number, vis: number) {
-            this.exercise_id = id;
-            this.exercise_name = name;
-            this.type_id = type;
-            this.exercise_description = descr;
-            this.exercise_sets = sets;
-            this.exercise_reps = reps;
-            this.exercise_duration = dura;
-            this.user_id = user;
-            this.visibility = vis;
-        }
+    // constructor (id: number, name: string, type: number, descr: string, sets: number,
+    //     reps: number, dura: number, user: number, vis: number) {
+    //         this.exercise_id = id;
+    //         this.exercise_name = name;
+    //         this.type_id = type;
+    //         this.exercise_description = descr;
+    //         this.exercise_sets = sets;
+    //         this.exercise_reps = reps;
+    //         this.exercise_duration = dura;
+    //         this.user_id = user;
+    //         this.visibility = vis;
+    //     }
 }
--- src/app/workout-landing/workout-landing.component.ts
+++ src/app/workout-landing/workout-landing.component.ts
@@ -4,6 +4,8 @@ import { Router } from '../../../node_modules/@angular/router';
 import { WorkoutService } from '../services/workout.service';
 import { User } from '../models/user';
 import { Modal } from '../../../node_modules/ngx-modialog/plugins/bootstrap';
+import { Exercise } from '../models/exercise';
+import { ExerciseService } from '../services/exercise.service';
 
 @Component({
   selector: 'app-workout-landing',
@@ -13,29 +15,59 @@ import { Modal } from '../../../node_modules/ngx-modialog/plugins/bootstrap';
 export class WorkoutLandingComponent implements OnInit {
 
   workouts: Workout[];
-  workout1: Workout = new Workout (1, 1, 1, 1, 'push-ups');
+  exercises: Exercise[];
+  // workout1: Workout = new Workout (1, 1, 1, 1, 'push-ups');
+  workout: Workout = new Workout();
   loggedUser: User = JSON.parse(localStorage.getItem('user'));
-  constructor(private router: Router, private workoutService: WorkoutService, public modal: Modal) { }
+  constructor(private router: Router, private workoutService: WorkoutService, public modal: Modal, 
+    private exerciseService: ExerciseService) { }
 
   ngOnInit() {
     // this.workoutService.getWorkoutByUserId(this.loggedUser.user_id).subscribe(response => {
     //   this.workouts = JSON.parse(<any>response);
     // });
-    this.workouts = [this.workout1];
+    this.workout.workout_name = 'pushups';
+    this.workout.user_id = 1;
+    this.workout.type_id = 1;
+    this.workouts = [this.workout];
   }
 
   startWorkout(workout: Workout) {
     this.workoutService.startWorkout(workout);
   }
 
+  getExercises(id: number) {
+    this.exerciseService.getExercisesByWorkoutId(id).subscribe(response => {
+      this.exercises = JSON.parse(<any>response);
+    });
+  }
+
+  getNumberOfExercises(id: number) {
+    let exercisesForWorkout: Exercise[];
+    this.exerciseService.getExercisesByWorkoutId(id).subscribe(response => {
+      exercisesForWorkout = JSON.parse(<any>response);
+    });
+    return exercisesForWorkout.length;
+  }
+
+  listExercises() {
+    let returnString: string;
+    for (let exercise of this.exercises) {
+      returnString.concat('<li>' + exercise.exercise_name + '</li>');
+    }
+    return returnString;
+  }
+
   openModal(workout: Workout) {
+    this.getExercises(workout.workout_id);
+    let numExercises = '<li>' + this.exercises.length + '</li>';
     this.modal.alert()
     .size('lg')
     .isBlocking(true)
     .showClose(true)
     .keyboard(27)
     .title(workout.workout_name)
-    .body('A Customized Modal')
+    .body('<ul>' + numExercises + this.listExercises() + '</ul>')
     .open();
   }
 
